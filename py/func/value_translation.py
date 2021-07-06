28c2cb282d796cfaa4219706123c0d13be4b06b3
Yusuke Kijima <yusukekijima@YusukenoMacBook-Pro.local>
2021-07-05 19:08:27 -0700

    bugfix

--- py/func/value_translation.py
+++ py/func/value_translation.py
@@ -144,9 +144,8 @@ def run(sampledir_list,cfg_raw,qcfg,is_qsub,is_multisample,param_dict,proj_dir,c
             print("Job failed: Building tree', file=sys.stderr")
             sys.exit(1)
     if is_qsub:
-        for sampledir in sampledir_list:
-            jid_now=cmd+param_dict[os.path.basename(sampledir)]["today_now"]
-            interstellar_setup.job_wait("Building tree",jid_now,sampledir+"/qlog",njob)
+        jid_now=cmd+param_dict[os.path.basename(sampledir)]["today_now"]
+        interstellar_setup.job_wait("Merging tree",jid_now,sampledir_list[0]+"/qlog",njob)
 
 
 
